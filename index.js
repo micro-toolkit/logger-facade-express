4038479b3e9ad8fa9ffe2d031697610cbb0c1bff
Pedro Januário <prnjanuario@gmail.com>
2016-04-11 08:30:16 -0300

    added ignore lint rule for compile function

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var middleware = require('./middleware');
 
+/* jshint ignore:start */
 function compile(format) {
   if (typeof format !== 'string') {
     throw new TypeError('argument format must be a string');
@@ -12,9 +13,9 @@ function compile(format) {
   };
   var regexp = /:([-\w]{2,})(?:\[([^\]]+)\])?/g;
   var js = '  return "' + fmt.replace(regexp, replacer) + '";';
-
   return new Function('tokens, req, res', js);
 }
+/* jshint ignore:end */
 
 var defaultFormat = ':id :remote-addr :method :url HTTP/:http-version :status' +
   ' :res[content-length] ":referrer" ":user-agent" - :response-time ms';
